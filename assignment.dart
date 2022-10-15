ec199363950fed951a27f657d024a609e663226b
shahzaibparwani <81838162+shahzaibparwani@users.noreply.github.com>
2022-10-16 01:41:07 +0500

    create assignment

---
+++ assignment.dart
@@ -0,0 +1,128 @@
+// Task 1
+// void main() {
+//    int length = 10;
+//    int breadth = 10;
+
+//   if(length == breadth){
+//     print("Square Value");
+//    }
+//    else { print("Rectangle Value");
+//   }
+// }
+
+// Task 2
+  // int shayan= 10;
+  // int shahnawaz= 50;
+  // int shahzaib = 20;
+
+  // if (shayan < shahnawaz) {
+  //   print("shayan is youngest");
+  // }
+  // if (shahnawaz > shahzaib) {
+  //   print("shahnawaz is Oldest than shahzaib and shayan");
+  // } else {
+  //   print("shahzaib youngest than shahnawaz and oldest than shayan");
+  // }
+
+// Task 3
+//   int totalClasses = 16;
+//   int attend = 10;
+//   num percent = attend / totalClasses * 100;
+//   print("your attendence $percent");
+//   if (percent >= 75) {
+//     print("allow ");
+//   } else {
+//     print("not allow because your attendence is $percent");
+// }
+
+// Task 4
+  // num year = 2000;
+  // if(year % 400 == 0 && year % 4 == 0) {
+  //   print("Leap Year");
+  // }
+  // else{
+  //   print("Not Leap Year");
+  // }
+
+
+// Task 5
+  // num temp = 42;
+  // if (temp < 0) {
+  //   print("Freezing Weather");
+  // }
+  // else if (temp > 0 && temp <= 10) {
+  //   print("Very Cold Weather");
+  // }
+  // else if (temp > 10 && temp <= 20) {
+  //   print("Cold Weather");
+  // }
+  // else if (temp > 20 && temp<= 30) {
+  //   print("Normal Weather");
+  // }
+  // else if (temp > 30 && temp<= 40) {
+  //   print("Hot Weather");
+  // }
+  // else if(temp >= 40) {
+  //   print("Very Hot Weather");
+  // }
+ 
+
+// Task 6
+  // var vowel1 = "A";
+  // var vowel2 = "E";
+  // var vowel3 = "I";
+  // var vowel4 = "O";
+  // var vowel5 = "U";
+  // var alpha = "U";
+  
+  // if (alpha == vowel1) {
+  //   print("A is Vowel");
+  // }
+  // else if(alpha == vowel2){
+  //   print("E is Vowel");
+  // }
+  // else if(alpha == vowel3){
+  //   print("I is Vowel");
+  // }
+  // else if(alpha == vowel4){
+  //   print("O isVowel");
+  // }
+  // else if(alpha == vowel5){
+  //   print("U is Vowel");
+  // }
+  // else{print("Consonant");}
+
+// Task 7
+  // var id = 1001;
+  // print("Customer IDNO: $id");
+
+  // var name = "James";
+  // print("Customer Name: $name");
+
+  // int unit = 800;
+  // print("Unit Consumed: $unit");
+
+  // if (unit < 200) {
+  //   num amount = unit * 1.2;
+  //   print("Bill Amount: $amount");
+  // }
+  // else if (unit >= 200 && unit < 400){    //because above 200 and less 400
+  //   num amount = unit * 1.5;
+  //   print("Bill Amount: $amount");
+  // }
+  // else if (unit >= 400 && unit < 600){
+  //   num amount = unit * 1.8;
+  //   print("Bill Amount: $amount");
+  // }
+  // else if (unit >= 600){
+  //   num amount = unit * 2.0;
+  //   print("Bill Amount: $amount");
+  // }
+
+  // } // main func end here
+
+  
+
+
+
+
