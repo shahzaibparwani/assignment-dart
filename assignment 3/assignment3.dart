1ff593ad0793f81d733508222f043dfd14038971
shahzaibparwani <81838162+shahzaibparwani@users.noreply.github.com>
2022-10-30 02:41:39 +0500

    create assignment 3

---
+++ assignment 3/assignment3.dart
@@ -0,0 +1,97 @@
+void main() {
+// Task 1
+
+  // List names = ["Shahzaib", "shayan", "shahnawaz", "khan", "bilal"];
+  // print(names[0]);
+  // print(names[1]);
+  // print(names[2]);
+  // print(names[3]);
+  // print(names[4]);
+
+// Task 2
+
+  // List<String> days = [];
+  // days.add("Monday");
+  // days.add("Tuesday");
+  // days.add("Wednesday");
+  // days.add("Thursday");
+  // days.add("Friday");
+  // days.add("Saturday");
+  // days.add("Sunday");
+  // print(days);
+
+//Task3
+
+  // List friends = [
+  //   "shahzaib",
+  //   "hamza",
+  //   "saad",
+  //   "bilal",
+  //   "anas",
+  //   "raza",
+  //   "zohaib"
+  // ];
+  // List names = friends.where((friend) => friend.startsWith("a")).toList();
+  // print(names);
+
+// Task4
+
+  // Map update = {
+  //   "name": "shahzaib",
+  //   "address": "old haji camp Karachi",
+  //   "age": 20,
+  //   "country": "america" };
+  //   update["country"] = "Pakistan";
+  //   print(update);
+
+// Task5
+
+  //   Map name = {"name1":"saad", "name2" : "shahzaib", "name3":"shayan"};
+  //   name.removeWhere((key,value) => value.length > 4);
+  //   print("length is 4 $name");
+
+// Task6
+
+ // Map world = {
+ //     "countries": [
+ //       {
+ //         "country1" : "pakistan",
+ //          "capital": 'islamabad',
+ //          "currency" : "rupees"
+ //       },
+ //       {
+ //         "country2" : "America",
+ //          "capital": 'Washington',
+ //          "currency" : "dollar"
+ //       }
+ //     ]
+ //   };
+ //  world.forEach((key, val) {
+ //   print(' $key, $val');
+ //   });
+
+// Task7
+
+  // Map<String, double> mathMarks = {
+  //   'ram': 30,
+  //   'mark': 32,
+  //   'harry': 88,
+  //   'raj': 69,
+  //   'john': 15,
+  // };
+
+  // mathMarks.removeWhere((key, value) => value <= 30);
+  // print(mathMarks);
+
+// Task8
+
+  //   Map<String, double> expenses = {
+  //   'sun': 3000.0,
+  //   'mon': 3000.0,
+  //   'tue': 3234.0,
+  // };
+  
+  //   var task = expenses.putIfAbsent("fri",()=> 5000.0);
+  //   print(expenses);
+
+}
